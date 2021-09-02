aa1caa6e9674424af70d68807f1e550688e3d240
yangqun <yangqun>
2021-09-02 16:40:13 +0800

    update website

--- website/main.ts
+++ website/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/styles/common.scss';
+import './assets/styles/fonts/style.css';
 import YUI from 'y-ui'
 import 'theme-chalk/src/index.scss'
 
--- website/pages/Index.vue
+++ website/pages/Index.vue
@@ -1,13 +1,17 @@
 <template>
 <div class="index-router">
-  <div class="components">Components:</div>
+  <div class="title">UI Components built on Vue 3</div>
+  <div class="components-title">Basic Components:</div>
   <div
     v-for="(item) in routes"
     :key="item.path"
     @click="goToComponent(item)"
     class="index-item"
   >
-   {{ item.name }}
+    <template v-if="item.name">
+      <span>{{ item.name }}</span>
+      <y-icon class="arrow-right-icon" name="arrow-right"></y-icon>
+    </template>
   </div>
 </div>
 </template>
@@ -31,17 +35,33 @@ export default defineComponent({
 </script>
 <style lang="scss" scoped>
 .index-router {
-  padding: 10px;
-  .components {
-    margin-bottom: 20px;
-    color: #e6a23c;
-    font-size: 48px;
+  padding: 46px 20px 20px;;
+  font-size: 14px;
+  .title {
+    margin: 0 0 40px;
+    padding-left: 16px;
+    font-size: 18px;
+  }
+  .components-title {
+    color: rgba(69, 90, 100, 0.6);
+    margin: 24px 0 8px 16px;
   }
   .index-item {
-    padding-bottom: 20px;
-    color: #99a9bf;
-    font-size: 32px;
-    text-decoration: underline;
+    margin: 0 0 12px;
+    padding-left: 20px;
+    color: #323233;
+    border-radius: 99px;
+    font-weight: 600;
+    line-height: 40px;
+    background: #f7f8fa;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .arrow-right-icon {
+      color: #B6C3D2;
+      position: relative;
+      right: 16px;
+    }
   }
 }
 </style>
